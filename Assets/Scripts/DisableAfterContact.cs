7c5ac6d10ff19c8ac54685378ff51df7201822af
Kalle Jillheden <kalle.jillheden@gmail.com>
2021-05-09 12:15:05 +0200

    Health disappears for 60 sec after picked up

---
+++ Assets/Scripts/DisableAfterContact.cs
@@ -0,0 +1,95 @@
+using UnityEngine;
+
+public class DisableAfterContact : MonoBehaviour
+{
+    public bool onCollision = true;
+    public bool onTrigger = false;
+    public bool shouldReenableAfterDelay = false;
+    public float reenableAfterSeconds = 30f;
+
+    public GameObject[] setActive = new GameObject[0];
+    public Behaviour[] setEnabled = new Behaviour[0];
+    public Collider[] colliders = new Collider[0];
+
+    [Header("These fields are set by script")]
+    public float secondsUntilReenabling = 0f;
+
+    public void Reset()
+    {
+        setActive = new[] { gameObject };
+        colliders = GetComponentsInChildren<Collider>();
+    }
+
+    public void OnCollisionEnter(Collision _)
+    {
+        if (onCollision)
+        {
+            SetEnabledState(false);
+        }
+    }
+
+    public void OnTriggerEnter(Collider _)
+    {
+        if (onTrigger)
+        {
+            SetEnabledState(false);
+        }
+    }
+
+    public void Update()
+    {
+        if (!shouldReenableAfterDelay || secondsUntilReenabling <= 0f)
+        {
+            return;
+        }
+
+        secondsUntilReenabling -= Time.deltaTime;
+
+        if (secondsUntilReenabling <= 0f)
+        {
+            SetEnabledState(true);
+            secondsUntilReenabling = 0f;
+        }
+    }
+
+    void SetEnabledState(bool enabled)
+    {
+        if (shouldReenableAfterDelay)
+        {
+            secondsUntilReenabling = reenableAfterSeconds;
+        }
+
+        if (setActive != null)
+        {
+            foreach (var gameObject in setActive)
+            {
+                if (gameObject)
+                {
+                    gameObject.SetActive(enabled);
+                }
+            }
+        }
+
+        if (setEnabled != null)
+        {
+            foreach (var behaviour in setEnabled)
+            {
+                if (behaviour)
+                {
+                    behaviour.enabled = enabled;
+                }
+            }
+        }
+
+        if (colliders != null)
+        {
+            foreach (var collider in colliders)
+            {
+                if (collider)
+                {
+                    collider.enabled = enabled;
+                }
+            }
+        }
+    }
+}
