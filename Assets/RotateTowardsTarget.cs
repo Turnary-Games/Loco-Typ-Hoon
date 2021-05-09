5d800adfd58d04ee4f9fd6ce088afdb3d76e258b
Kalle Jillheden <kalle.jillheden@gmail.com>
2021-05-09 12:46:06 +0200

    Error handling in RotateTowardsTarget

--- Assets/RotateTowardsTarget.cs
+++ Assets/RotateTowardsTarget.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class RotateTowardsTarget : MonoBehaviour
@@ -8,7 +6,17 @@ public class RotateTowardsTarget : MonoBehaviour
 
     public float speed = 1.0f;
 
-    void Update()
+    public void OnEnable()
+    {
+        if (!target)
+        {
+            Debug.LogWarning($"The '{nameof(target)}' field must be set.", this);
+            enabled = false;
+            return;
+        }
+    }
+
+    public void Update()
     {
         var q = Quaternion.LookRotation(target.position - transform.position);
         transform.rotation = Quaternion.RotateTowards(transform.rotation, q, speed * Time.deltaTime);
